92caeb8bee91efdc420435d8fc2b2fa91a0e2bc7
Celio Caus Junior <ccaus@uol.com.br>
2020-01-04 13:49:41 -0300

    findAll Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,7 +30,13 @@ public class Program {
 			System.out.println(obj);
 		}
 	
-	
+System.out.println("\n======= TEST 3 : Seller find All ========");
+		
+				
+		list = sellerDao.findAll();
+		for (Seller obj:list) {
+			System.out.println(obj);
+		}
 	}
 
 }
