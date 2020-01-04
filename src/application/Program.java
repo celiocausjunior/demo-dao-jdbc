53868a83c3c8030c573d61097204597cbe93c8c2
Celio Caus Junior <ccaus@uol.com.br>
2020-01-04 15:32:39 -0300

    Insert Implemetation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -10,33 +11,35 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
-		
-		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
-		System.out.println("======= TEST 1 : Seller find By Id ========");
-		
-		Seller seller = sellerDao.findById(3);
-		
-		System.out.println(seller);
-		
-		System.out.println("\n======= TEST 2 : Seller find By Department ========");
-		
-		Department department = new Department(2, null);
-		
-		
-		List<Seller> list = sellerDao.findByDepartment(department);
-		for (Seller obj:list) {
-			System.out.println(obj);
-		}
-	
-System.out.println("\n======= TEST 3 : Seller find All ========");
-		
-				
-		list = sellerDao.findAll();
-		for (Seller obj:list) {
-			System.out.println(obj);
-		}
-	}
 
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+
+		System.out.println("======= TEST 1 : Seller find By Id ========");
+
+		Seller seller = sellerDao.findById(3);
+
+		System.out.println(seller);
+
+		System.out.println("\n======= TEST 2 : Seller find By Department ========");
+
+		Department department = new Department(2, null);
+
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+
+		System.out.println("\n======= TEST 3 : Seller find All ========");
+
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+
+		System.out.println("\n======= TEST 4 : Seller Insert ========");
+
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+	}
 }
