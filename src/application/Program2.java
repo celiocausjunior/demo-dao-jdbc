348046fd7697163b388be271b65fbbff5cb44ff1
Celio Caus Junior <ccaus@uol.com.br>
2020-01-05 19:21:02 -0300

    Insert Department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.entities.Department;
 
 public class Program2 {
@@ -18,13 +19,22 @@ public class Program2 {
 
 		System.out.println(depto);
 
-		System.out.println("======= TEST 1 : Departmentfind By Id ========");
+		System.out.println("======= TEST 2 : Department find All ========");
 
 		List<Department> list = deptoDao.findAll();
 		for (Department obj : list) {
 			System.out.println(obj);
 		}
 
+		System.out.println("======= TEST 3 : Insert Department ========");
+
+		Department newDepartment = new Department(null, "Watches" );
+		
+		deptoDao.insert(newDepartment);
+		
+		System.out.println("Inserted!  New id = " + newDepartment.getId());
+		
+	
 	}
 
 }
