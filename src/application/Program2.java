c2bf44f5c17449b2f50914515e0daadc57821863
Celio Caus Junior <ccaus@uol.com.br>
2020-01-05 00:07:58 -0300

    Find All Departments

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -8,11 +10,20 @@ public class Program2 {
 
 	public static void main(String[] args) {
 
-	DepartmentDao deptoDao = DaoFactory.createDepartmentDao();
-	
-	Department depto = deptoDao.findById(1);
-	
-	System.out.println(depto);
+		DepartmentDao deptoDao = DaoFactory.createDepartmentDao();
+
+		System.out.println("======= TEST 1 : Departmentfind By Id ========");
+
+		Department depto = deptoDao.findById(1);
+
+		System.out.println(depto);
+
+		System.out.println("======= TEST 1 : Departmentfind By Id ========");
+
+		List<Department> list = deptoDao.findAll();
+		for (Department obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
